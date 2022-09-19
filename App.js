e9a7a5ff538ab380d756c11a3a40bc50e8c875a7
Igor Vinícius <102437841+igorvinicius1@users.noreply.github.com>
2022-09-19 16:27:11 -0300

    Adicionei Tela de cadastro da Easy Fretes

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, Button, TouchableOpacity,} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <View>
+          <Image style={styles.logo} source={require('./assets/Topo.png')}/>
+      </View>
+
       <StatusBar style="auto" />
+
+      <View>
+        <Image style={styles.frame} source={require('./assets/Frame1.png')}/>
+      </View>
+    
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.letra}>Cadastrar</Text>
+      </TouchableOpacity>
+      
+      <View><Text style={styles.letra2}>Entrar</Text></View>
+      
     </View>
   );
 }
@@ -13,8 +28,55 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgba(255, 197, 110, 1)',
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'column',
   },
+
+  logo:
+  {
+    width: 350,
+    height: 180,
+    marginRight: 10,
+    paddingBottom: 180,
+  },
+
+  frame:
+  {
+    width: 380,
+    marginTop: 70,
+  },
+
+
+  letra:
+  {
+    color: '#fff',
+    fontWeight: '700',
+    fontSize: 18,
+  },
+
+  letra2:
+  {
+    color: '#fff',
+    fontWeight: '700',
+    fontSize: 18,
+    textDecorationLine: 'underline'
+  },
+
+  button:
+  {
+    backgroundColor: 'rgba(255, 108, 1, 1)',
+    color: '#fff',
+    width: 350,
+    height: 55,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 55,
+    marginBottom: 180,
+    marginTop: 90,
+    shadowColor: 'black',
+    elevation: 3,
+  },
+
 });
